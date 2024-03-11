89cf0b940b8d12a2a3454db6a88a3597b69736c9
AlexanderFKratz <49705346+afkratz@users.noreply.github.com>
2024-03-11 11:52:30 -0700

    Added supp table 1 and added linux support to 01_run_uclust

--- scripts/process_prescreen/01_run_uclust.py
+++ scripts/process_prescreen/01_run_uclust.py
@@ -9,47 +9,133 @@ MIT license
 
 import os
 from pathlib import Path
-os.chdir(Path(__file__).resolve().parent.parent.parent)
+import hashlib
+import gzip
+import requests
 import pandas as pd
+import shutil
 
-#Find path to useach
-#The license is ambiguous on if it'd be ok to redistribute the binary
-#Regardless, for compatibility with whatever OS you are using, 
-# we allow/require you to supply the usearch executable  
 
-path_to_uclust = "../uclust/usearch.exe" #Personally where I placed it
+def download_file(url,filename):
+    response = requests.get(url,stream=True)
+    with open(filename,'wb') as f:
+        shutil.copyfileobj(response.raw, f)
 
-while not os.path.exists(path_to_uclust):
-    path_to_uclust = input("Please enter path to usearch executable ")
-path_to_uclust = os.path.abspath(path_to_uclust)
+def decompress_gzip(filename):
+    with gzip.open(filename,'rb') as f_in:
+        with open(filename[:-3], 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    os.remove(filename)
 
-#Make paths for output if they don't exist
-if not os.path.exists(os.path.join("output","prescreen_results")):
-    os.mkdir(os.path.join("output","prescreen_results"))
+def calculate_md5(filename):
+    with open(filename, 'rb') as f:
+        md5_hash = hashlib.md5()
+        for chunk in iter(lambda: f.read(4096), b""):
+            md5_hash.update(chunk)
+        return md5_hash.hexdigest()
+uclust_path = ""
+def download_uclust():
+    ChavezCIRSPRa_root_dir  = Path(__file__).resolve().parent.parent.parent
+    if os.name =='nt':#We are in windows
+        uclust_path = os.path.join(ChavezCIRSPRa_root_dir,'uclust.exe')
+        if not os.path.exists(uclust_path):
+            download_file(
+                'https://drive5.com/downloads/usearch11.0.667_win32.exe.gz',
+                uclust_path+'.gz'
+                )
+            expected_md5='fd5e149d0977a570cdaa10652a994d45'
+            downloaded_file_md5 = calculate_md5(uclust_path+'.gz')
+            if downloaded_file_md5 != expected_md5:
+                print("MD5 for uclust does not match expected value...")
+                r='x'
+                while r not in 'qcQC':
+                    r = input("Quit or continue? Q/C:")
+                if r in 'Qq':
+                    print("Quitting")
+                    exit()
+                else:
+                    print("Continuing with un-verified uclust.exe")
+            decompress_gzip(uclust_path+'.gz')
+            return 
+    if os.name == 'posix':
+        uclust_path = os.path.join(ChavezCIRSPRa_root_dir,'uclust')
+        if not os.path.exists(uclust_path):
+            download_file(
+                'https://drive5.com/downloads/usearch11.0.667_i86linux32.gz',
+                uclust_path+'.gz'
+                )
+            expected_md5='27cc6b779b899daa5d17a95fd0eeb69c'
+            downloaded_file_md5 = calculate_md5(uclust_path+'.gz')
+            if downloaded_file_md5 != expected_md5:
+                print("MD5 for uclust does not match expected value...")
+                r='x'
+                while r not in 'qcQC':
+                    r = input("Quit or continue? Q/C:")
+                if r in 'Qq':
+                    print("Quitting")
+                    exit()
+                else:
+                    print("Continuing with un-verified uclust.exe")
+            decompress_gzip(uclust_path+'.gz')
+            os.chmod(filename, os.stat(uclust_path).st_mode | 0o111)
+            return 
+    else:
+        print("Sorry, we only natively support windows or linux")
+        quit()
 
-if not os.path.exists(os.path.join("output","prescreen_results","clusters")):
-    os.mkdir(os.path.join("output","prescreen_results","clusters"))
+def call_uclust():
+    ChavezCIRSPRa_root_dir  = Path(__file__).resolve().parent.parent.parent
+    if os.name =='nt':#We are in windows
+        uclust_path = os.path.join(ChavezCIRSPRa_root_dir,'uclust.exe')
 
-#Load input dataframe
-df = pd.read_excel(
-    os.path.join("input_data","Supplementary Table 1. Domain origins and sequences.xlsx"),
-    index_col = "Domain ID"
+    elif os.name =='posix':#We are in windows
+        uclust_path = os.path.join(ChavezCIRSPRa_root_dir,'uclust')
+
+    else:
+        print("Sorry, I only wrote the code to support windows or linux")
+        """
+        Feel free to edit this file to point the varialbe uclust_path to where ever you have uclust installed
+        """
+        raise NotImplementedError()
+
+
+    #Make paths for output if they don't exist
+    if not os.path.exists(os.path.join(ChavezCIRSPRa_root_dir,"output","prescreen_results")):
+        os.mkdir(os.path.join(ChavezCIRSPRa_root_dir,"output","prescreen_results"))
+
+    if not os.path.exists(os.path.join(ChavezCIRSPRa_root_dir,"output","prescreen_results","clusters")):
+        os.mkdir(os.path.join(ChavezCIRSPRa_root_dir,"output","prescreen_results","clusters"))
+
+    #Load input dataframe
+    df = pd.read_excel(
+        os.path.join(ChavezCIRSPRa_root_dir,"input_data","Supplementary Table 1. Domain origins and sequences.xlsx"),
+        index_col = "Domain ID"
+        )
+
+    #Drop protein folding domains
+    df = df[df["Role"]=="Activator"]
+
+
+    #Write sequences to a .fasta for clustering
+    with open(
+            os.path.join(ChavezCIRSPRa_root_dir,"sequences_to_cluster.fasta"),
+            'w') as fh:
+        for i in df.index:
+            fh.write(">{}\n".format(i))
+            fh.write(df.at[i,"AA sequence"]+"\n")
+
+    #Run uclust
+    uclust_command = uclust_path +" -cluster_fast {} -id 0.5 -centroids {} -clusters {}".format(
+        os.path.join(ChavezCIRSPRa_root_dir,"sequences_to_cluster.fasta"),
+        os.path.join(ChavezCIRSPRa_root_dir,"output","prescreen_results","centroids.fasta"),
+        os.path.join(ChavezCIRSPRa_root_dir,"output","prescreen_results","clusters","cluster"),
     )
 
-#Drop protein folding domains
-df = df[df["Role"]=="Activator"]
+    os.system(uclust_command)
 
+    #Clean up after ourselves
+    os.remove(os.path.join(ChavezCIRSPRa_root_dir,"sequences_to_cluster.fasta"))
 
-#Write sequences to a .fasta for clustering
-with open("sequences_to_cluster.fasta",'w') as fh:
-    for i in df.index:
-        fh.write(">{}\n".format(i))
-        fh.write(df.at[i,"AA sequence"]+"\n")
-
-#Run uclust
-uclust_command = path_to_uclust +" -cluster_fast sequences_to_cluster.fasta -id 0.5 -centroids output/prescreen_results/centroids.fasta -clusters output/prescreen_results/clusters/"
-
-os.system(uclust_command)
-
-#Clean up after ourselves
-os.remove("sequences_to_cluster.fasta")
+if __name__=="__main__":
+    download_uclust()
+    call_uclust()
--- scripts/process_prescreen/validate_positions.py
+++ scripts/process_prescreen/validate_positions.py
@@ -8,16 +8,9 @@ MIT license
 """
 import os
 from pathlib import Path
-import sys
 import pandas as pd
 import requests
-
-
-os.chdir(Path(__file__).resolve().parent.parent.parent)
-sys.path.insert(0,str(Path(__file__).resolve().parent.parent.parent))
-
-df = pd.read_excel('input_data\Supplementary Table 1. Domain origins and sequences.xlsx',index_col='Domain ID')
-
+import math
 
 def download_fasta(uniprot_id):
     url = f"https://rest.uniprot.org/uniprotkb/{uniprot_id}.fasta"
@@ -33,41 +26,55 @@ def download_fasta(uniprot_id):
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
-for i in df.index:
-    print(i)
-    if df.at[i,'Designed or found']=='Found':
-        uniprot_name = df.at[i,'UniProt']
-        if not isinstance(uniprot_name,str):continue #Checks for NANs
-        seq = download_fasta(uniprot_name)
-        if i not in ['BAHV1_ICP4','BPV1_NS1','EC_MBPMUT','EC_MBPWT','EF_D920_01889','HS_ATF7','HS_P65']:
-            start_aa = seq.find(df.at[i,'AA sequence'])
-            df.at[i,'recalc start']=(start_aa)/len(seq)
-            df.at[i,'recalc end']=((start_aa)+len(df.at[i,'AA sequence']))/len(seq)
-        
 
-        #A few activators have slight differences from uniprot that we handle manually
-        if i=='BAHV1_ICP4': 
-            #Our is missing 1aa compared to uniprot
-            our_sequence = df.at[i,'AA sequence']
-            original_sequence  = our_sequence[:59]+'R'+our_sequence[59:]
-            start_aa = seq.find(original_sequence)
-            df.at[i,'recalc start']=(start_aa)/len(seq)
-            df.at[i,'recalc end']=((start_aa)+len(original_sequence))/len(seq)
-        
-        if i=='HS_P65':
-            #Our version of P65 came from a construct with one extra Serine in the GS-linker
-            our_sequence = df.at[i,'AA sequence']
-            original_sequence = our_sequence[1:]#skip first AA
-            start_aa = seq.find(original_sequence)
-            df.at[i,'recalc start']=(start_aa)/len(seq)
-            df.at[i,'recalc end']=((start_aa)+len(original_sequence))/len(seq)
+def validate_start_and_stop_positions():
+    ChavezCIRSPRa_root_dir = Path(__file__).resolve().parent.parent.parent
+    
+    df = pd.read_excel(
+        os.path.join(
+            ChavezCIRSPRa_root_dir,
+            'input_data',
+            'Supplementary Table 1. Domain origins and sequences.xlsx'),index_col='Domain ID'
+        )
+    for i in df.index:
+        if df.at[i,'Designed or found']=='Found':
+            print(i+"...",end="")
+            uniprot_name = df.at[i,'UniProt']
+            if not isinstance(uniprot_name,str):continue #Checks for NANs
+            seq = download_fasta(uniprot_name)
+            if i not in ['BAHV1_ICP4','BPV1_NS1','EC_MBPMUT','EC_MBPWT','EF_D920_01889','HS_ATF7','HS_P65']:
+                start_aa = seq.find(df.at[i,'AA sequence'])
+                re_calculated_start=(start_aa)/len(seq)
+                re_calculated_end=((start_aa)+len(df.at[i,'AA sequence']))/len(seq)
+            
+            #A few activators have slight differences from uniprot that we handle manually
+            if i=='BAHV1_ICP4': 
+                #Our is missing 1aa compared to uniprot
+                our_sequence = df.at[i,'AA sequence']
+                original_sequence  = our_sequence[:59]+'R'+our_sequence[59:]
+                start_aa = seq.find(original_sequence)
+                re_calculated_start=(start_aa)/len(seq)
+                re_calculated_end=((start_aa)+len(original_sequence))/len(seq)
+            
+            if i=='HS_P65':
+                #Our version of P65 came from a construct with one extra Serine in the GS-linker
+                our_sequence = df.at[i,'AA sequence']
+                original_sequence = our_sequence[1:]#skip first AA
+                start_aa = seq.find(original_sequence)
+                re_calculated_start=(start_aa)/len(seq)
+                re_calculated_end=((start_aa)+len(original_sequence))/len(seq)
 
-        if i=='BPV1_NS1':
-            #Our version has a 1aa substitution compared to uniprot, R742G
-            our_sequence = df.at[i,'AA sequence']
-            original_sequence = our_sequence[:116]+'R'+our_sequence[117:]
-            start_aa = seq.find(original_sequence)
-            df.at[i,'recalc start']=(start_aa)/len(seq)
-            df.at[i,'recalc end']=((start_aa)+len(original_sequence))/len(seq)
+            if i=='BPV1_NS1':
+                #Our version has a 1aa substitution compared to uniprot, R742G
+                our_sequence = df.at[i,'AA sequence']
+                original_sequence = our_sequence[:116]+'R'+our_sequence[117:]
+                start_aa = seq.find(original_sequence)
+                re_calculated_start=(start_aa)/len(seq)
+                re_calculated_end=((start_aa)+len(original_sequence))/len(seq)
 
-df.to_csv('Supplementary Table 1. Domain origins and sequences with revalidated startstop.csv')
+            assert math.isclose(re_calculated_start, df.at[i,'Origin protein start position'],abs_tol=0.0001)
+            assert math.isclose(re_calculated_end, df.at[i,'Origin protein end position'],abs_tol=0.0001)
+            print("True")
+
+if __name__=="__main__":
+    validate_start_and_stop_positions()
